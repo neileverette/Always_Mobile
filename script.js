afa3285d2e4cc26ce64dd228c8803af6aa47ec83
Neil Everette <neil.everette@gmail.com>
2013-05-19 12:42:09 +0900

    Initial commit of the project

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	console.log("This works");
+	console.log($(document).height());
+	console.log($(window).height());
+
+	//$('#Div1').css(line - height, '200px');
+	//$( "#Div1" ).css( "border","3px solid red" )
+
+
+});
