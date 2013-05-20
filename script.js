34efc2d1ddbf106554c9e58f08c7ca7541c3b291
Neil Everette <neil.everette@gmail.com>
2013-05-20 16:04:54 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-$(document).ready(function(){
-	console.log("This works");
-	console.log($(document).height());
-	console.log($(window).height());
+$(document).ready(function () {
+//    $('.imageGradient').css('height', $(window).height() * .14);
+    $('#Div1').css('height', $(window).height() * .14);
+    $('#Div2').css('height', $(window).height() * .14);
+    $('#Div3').css('height', $(window).height() * .44);
+    $('#Div4').css('height', $(window).height() * .14);
+    $('#Div5').css('height', $(window).height() * .14);
+});
 
-	//$('#Div1').css(line - height, '200px');
-	//$( "#Div1" ).css( "border","3px solid red" )
-
+$(window).resize(function () {
+//    $('.imageGradient').css('height', $(window).height() * .14);
+    $('#Div1').css('height', $(window).height() * .14);
+    $('#Div2').css('height', $(window).height() * .14);
+    $('#Div3').css('height', $(window).height() * .44);
+    $('#Div4').css('height', $(window).height() * .14);
+    $('#Div5').css('height', $(window).height() * .14);
 
 });
+
