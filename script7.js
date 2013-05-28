d685747ffb610c55d9f046d37b069daf1ca266de
Neil Everette <neil.everette@gmail.com>
2013-05-28 05:58:06 -0700

    Basic filter working

--- script7.js
+++ script7.js
@@ -1,19 +1,10 @@
 $(document).ready(function () {
-	fadeOut(); // fades the header out
-	fadeIn();
+	
+	$('#filter').css('display', 'none');
+	$('#dotOne').click(function() {
+		$('#filter').toggle('slide', 500);
+	});
 });
 
 
-function fadeOut(){
-	$('.title').delay(3000).fadeTo(1000, 0);
-	//fadeIn();
-}
 
-function fadeIn(){
-	$(document).click(function(){
-	$('.title').fadeTo(1500, 100 );
-	fadeOut();
-	})
-	
-	
-	}
